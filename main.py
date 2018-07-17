2237eb10d31685f2a720cfad2983dd37c6de34a7
Jeff Lyons <jeffkt95@gmail.com>
2018-07-17 15:32:48 -0400

    Commented out the MintConnection stuff from the third party lib. It's not working.

--- main.py
+++ main.py
@@ -2,32 +2,35 @@ import Utilities
 from Accounts import Accounts
 from AllMoneySpreadsheet import AllMoneySpreadsheet
 from MintAccountsNameMap import MintAccountsNameMap
-from Accounts import ImproperlyFormattedMintData
 from MintConnection import MintConnection
+from Accounts import ImproperlyFormattedMintData
 import StockData
 import webbrowser
 from builtins import input
+#import getpass
 
 #Besides running the allMoneyScript, this method runs all the stuff you would do manually before and after
 def main():
+    webbrowser.open("https://mint.intuit.com/overview.event")
+    wait = input("Copy account data from mint.com then press enter.")
     allMoneySpreadsheet = allMoneyScript(True)
     wait = input("All done!")
-    
     
 #This script grabs what's on the clipboard from a mint.com copy and puts it into the AllMoney spreadseheet in the right place.
 #It also grabs the dow.
 def allMoneyScript(launchSpreadsheet = False):
-    mintConnection = MintConnection("jeffkt@alum.mit.edu", password)
+    accountsCopiedFromMint = Utilities.getClipboard()
+    #password = getpass.getpass(prompt="Enter Mint password: ")
+    #mintConnection = MintConnection("jeffkt@alum.mit.edu", password)
 
     accountsFromMint = Accounts()
-    #try:
-    #    accountsFromMint.getAccountsFromMintCopy(accountsCopiedFromMint)
-    #except ImproperlyFormattedMintData as err:
-    #    print("Error: " + err.message)
-    #    print("Quitting.")
-    #    return
-    
-    accountsFromMint.getAccountsFromMintAccounts(mintConnection.getAccounts())
+    #accountsFromMint.getAccountsFromMintAccounts(mintConnection.getAccounts())
+    try:
+        accountsFromMint.getAccountsFromMintCopy(accountsCopiedFromMint)
+    except ImproperlyFormattedMintData as err:
+        print("Error: " + err.message)
+        print("Quitting.")
+        return
     
     print(accountsFromMint)
     
